c074e5f6c1fbc71b186faf30fbe57232a1110780
Alex MacCaw <alex@alexmaccaw.com>
2014-12-08 16:16:19 -0800

    gitignore build

--- build/lib/clearbit/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from clearbit.error import (ClearbitError, ParamsInvalidError)
-from clearbit.person import Person
-from clearbit.company import Company
-from clearbit.person_company import PersonCompany
-
-key = os.getenv('CLEARBIT_KEY', None)

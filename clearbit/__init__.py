f7d624b8caf398910a1b201122916048bbc551be
Nogah <nogah@cherrypick-consulting.com>
2022-04-28 14:30:20 +0300

    fix import

--- clearbit/__init__.py
+++ clearbit/__init__.py
@@ -6,11 +6,11 @@ from .enrichment import Enrichment
 from .enrichment import Company
 from .enrichment import Person
 from .enrichment import PersonCompany
-from prospector import Prospector
+from .prospector import Prospector
 from clearbit.resource import Resource
-from reveal import Reveal
-from risk import Risk
-from name_to_domain import NameToDomain
+from .reveal import Reveal
+from .risk import Risk
+from .name_to_domain import NameToDomain
 from clearbit.watchlist import Watchlist
 from clearbit.watchlist import Entity as WatchlistEntity
 from clearbit.watchlist import Individual as WatchlistIndividual
